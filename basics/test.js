bcf9bae80945d72afe62e521823bfa92f63407e4
Maaz Rahim Khan <117745292+maaz-321@users.noreply.github.com>
2024-03-12 18:53:52 +0000

    conversionofvalues

---
+++ basics/test.js
@@ -0,0 +1,60 @@
+
+//d=null;
+//console.log(typeof d);
+
+
+//conversion of string into number
+
+ n="4839gvs";
+ console.log(n);
+ changenumber=Number(n);
+ console.log(changenumber);
+
+ console.log(typeof changenumber);
+
+
+//use for null value
+
+value=null;
+console.log(value);
+changenullvalue=Number(value);
+console.log(changenullvalue);
+console.log(typeof changenullvalue)
+
+//null value change into a zero and the type of object convert into a number
+
+
+
+undefinevalue=undefined;
+console.log(undefined);
+changevalues=Number(undefinevalue);
+console.log(changevalues);
+console.log(typeof changevalues);
+
+//over all the string null and undefined is change into a numbers
+//but String is not change into a number change into Nan
+
+
+
+a=1;
+
+change_a=Boolean(a);
+
+console.log(change_a);
+
+b="value";
+
+change_b=Boolean(b);
+
+console.log(change_b);
+
+c=null;
+
+change_c=Boolean(c);
+
+console.log(change_c);
+
+
+//true=>1, false=>0
+//""=>false, values=>1
+//null=>false
